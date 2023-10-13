a6345e572d71d3666920ca36fda28fc8fd82e99a
Robert <robertdahora@gmail.com>
2023-10-13 08:03:48 -0300

    construindo header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, GlobalCss } from './styles'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <Container>
+        <Header />
+      </Container>
+    </>
+  )
+}
+
+export default App
